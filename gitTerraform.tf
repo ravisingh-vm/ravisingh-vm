0bcac83bd077ffb941a874bd177c47e129db73ad
Ravi <inbox.ravi25@gmail.com>
2022-08-12 12:41:59 +0530

    Terraform Repo Code

---
+++ gitTerraform.tf
@@ -0,0 +1,15 @@
+
+/*
+provider "github" {
+  # Configuration options
+  token = "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"
+}
+
+
+resource "github_repository" "example" {
+  name = "terraformREPO"
+
+  visibility = "public"
+
+}
+*/
